369315123356e52ab81bbe4fcb5cdf1f6bc6ec10
jbergmann <jbergmann@sztaki.hu>
2023-08-09 15:33:21 +0200

    update environment: more elegant print

--- environment.py
+++ environment.py
@@ -1,10 +1,14 @@
 # Imports
+from itertools import product
 from random import shuffle
 
 # Constants
 SYMBOLS = ["C", "D", "H", "S"]
 COLORS = {"C": 1, "D": 0, "H": 0, "S": 1}
+NOTES = range(1, 14)
+DECK = list(product(SYMBOLS, NOTES))
 
+NUM_COLS = 7
 
 # Classes
 class Card:
@@ -16,18 +20,17 @@ class Card:
         self.movable = False
 
 
-class Table:
+class Table(object):
     def __init__(self):
         deck = []
-        for symbol in SYMBOLS:
-            for note in range(1, 14):
-                deck.append(Card(symbol, note))
-        # shuffle(deck)
+        for symbol, note in DECK:
+            deck.append(Card(symbol, note))
+        shuffle(deck)
         self.final_deck = {symbol: [] for symbol in SYMBOLS}
         self.play_space = []
-        for i in range(7):
+        for i in range(NUM_COLS):
             self.play_space.append([])
-            for j in range(i + 1):
+            for _ in range(i + 1):
                 self.play_space[-1].append(deck[0])
                 deck.pop(0)
         self.leftover = deck
@@ -37,29 +40,40 @@ class Table:
             column[-1].is_known = True
             column[-1].movable = True
 
-    def print_table(self):
+    def __str__(self):
+        layout = "\nDEAL:\n"
         for column in self.play_space:
-            print(
-                [
-                    (elem.symbol, elem.note) if elem.is_known else ("?", "?")
-                    for elem in column
-                ]
-            )
-        print(
-            [
-                (elem.symbol, elem.note) if elem.is_known else ("?", "?")
-                for elem in self.leftover
-            ]
-        )
+            for elem in column:
+                if elem.is_known:
+                    layout += elem.symbol
+                    layout += str(elem.note)
+                    layout += " "
+                else:
+                    layout += "?? "
+            layout += "\n"
+            
+        layout += "\nLEFTOVER:\n"
+        for elem in self.leftover:
+            if elem.is_known:
+                layout += elem.symbol
+                layout += str(elem.note)
+                layout += " "
+            else:
+                layout += "?? "
+        layout += "\n"
+
+        layout += "\nTOP ROW:\n"
         for shape, finals in self.final_deck.items():
-            print(shape)
-            print(
-                [
-                    (elem.symbol, elem.note) if elem.is_known else ("?", "?")
-                    for elem in finals
-                ]
-            )
-        return None
+            layout += shape + ": "
+            for elem in finals:
+                if elem.is_known:
+                    layout += elem.symbol
+                    layout += str(elem.note)
+                    layout += " "
+                else:
+                    layout += "?? "
+            layout += "\n"
+        return layout
 
     def get_valid_actions(self):
         """
@@ -68,7 +82,7 @@ class Table:
         valid_actions = []
         # check finals
         destination = "final_deck"
-        for i in range(7):
+        for i in range(NUM_COLS):
             card = self.play_space[i][-1]
             if check_valid_move(card=card, table=self, destination=destination):
                 valid_actions.append((card, destination))
@@ -94,5 +108,5 @@ def check_valid_move(card: Card, table: Table, destination: str):
 
 
 T = Table()
-T.print_table()
-T.get_valid_actions()
+print(T)
+print(T.get_valid_actions())
