98c2a308af1f2a13520e6b80a71c8c8d46ae6fc0
jbergmann <jbergmann@sztaki.hu>
2023-08-09 10:10:27 +0200

    create reqs batch file

---
+++ requirements.bat
@@ -0,0 +1,6 @@
+pip install pip-tools 
+pip freeze > "requirements.in" 
+pip-compile -o requirements.txt requirements.in 
+del .\requirements.in 
+git.exe add .\requirements.txt 
+git.exe commit -m "update requirements" 
